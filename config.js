85d05937ce4a3f7124c7b5ebb1a90457d1e51bfc
Julien Nicolas <julien.brcht.rndlt@gmail.com>
2021-10-14 16:43:48 +0200

    ftpconfig

---
+++ config.js
@@ -0,0 +1,11 @@
+require('dotenv').config();
+
+const options = {
+    host: process.env.FTP_HOST,
+    port: process.env.FTP_PORT,
+    user: process.env.FTP_USER,
+    password: process.env.FTP_PASSWORD,
+    secure: false
+};
+
+module.exports = options;
